6d622b7117b5c39c2b5af5f80279f3ed8df7bbb6
fwdAndroid <fwdkaleem@gmail.com>
2024-12-25 00:05:37 +0500

    Space

--- lib/user_exist_profle/profile_page_2.dart
+++ lib/user_exist_profle/profile_page_2.dart
@@ -151,6 +151,7 @@ class _ProfilePage2State extends State<ProfilePage2> {
               ),
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 SizedBox(
                   width: 200,
