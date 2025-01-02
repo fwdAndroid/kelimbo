cf37f5d79f4512351a8200fc974a7e7db72180f1
fwdAndroid <fwdkaleem@gmail.com>
2025-01-02 17:18:58 +0500

    Issue SOlved

--- lib/screens/main/other/second_user_profile.dart
+++ lib/screens/main/other/second_user_profile.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kelimbo/screens/hiring/hiring_service.dart';
@@ -33,6 +34,8 @@ class _SecondUserProfileState extends State<SecondUserProfile> {
     super.initState();
     print(widget.userId);
   }
+
+  final currentUserId = FirebaseAuth.instance.currentUser!.uid;
 
   @override
   Widget build(BuildContext context) {
@@ -103,127 +106,154 @@ class _SecondUserProfileState extends State<SecondUserProfile> {
                   );
                 }
                 return Expanded(
-                  child: ListView.builder(
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        final List<DocumentSnapshot> documents =
-                            snapshot.data!.docs;
-                        final Map<String, dynamic> data =
-                            documents[index].data() as Map<String, dynamic>;
-                        return Card(
+                    child: ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    final data = snapshot.data!.docs[index].data()
+                        as Map<String, dynamic>;
+
+                    bool isFavorite = (data['favorite'] as List<dynamic>?)
+                            ?.contains(currentUserId) ??
+                        false;
+
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (builder) => HiringService(
+                                      serviceId: data['uuid'],
+                                      currencyType: data['currency'],
+                                      userEmail: data['userEmail'],
+                                      userImage: data['userImage'],
+                                      userName: data['userName'],
+                                      category: data['category'],
+                                      totalReviews:
+                                          data['totalReviews'].toString(),
+                                      uuid: data['uuid'],
+                                      uid: data['uid'],
+                                      totalRating: data['totalRate'].toString(),
+                                      title: data['title'],
+                                      price: data['price'].toString(),
+                                      perHrPrice: data['pricePerHr'].toString(),
+                                      photo: data['photo'],
+                                      description: data['description'],
+                                    )));
+                      },
+                      child: SizedBox(
+                        height: 300,
+                        width: 300,
+                        child: Card(
                           child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              ListTile(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (builder) => HiringService(
-                                                userEmail: data['userEmail'],
-                                                userImage: data['userImage'],
-                                                userName: data['userName'],
-                                                category: data['category'],
-                                                totalReviews:
-                                                    data['totalReviews']
-                                                        .toString(),
-                                                uuid: data['uuid'],
-                                                uid: data['uid'],
-                                                currencyType: data['currency'],
-                                                totalRating: data['totalRate']
-                                                    .toString(),
-                                                title: data['title'],
-                                                price: data['price'].toString(),
-                                                serviceId: data['uuid'],
-                                                perHrPrice: data['pricePerHr']
-                                                    .toString(),
-                                                photo: data['photo'],
-                                                description:
-                                                    data['description'],
-                                              )));
-                                },
-                                leading: data['photo'] == ""
-                                    ? CircleAvatar()
-                                    : CircleAvatar(
-                                        backgroundImage:
-                                            NetworkImage(data['photo']),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Stack(
+                                  children: [
+                                    data['photo'] == ""
+                                        ? Container()
+                                        : ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                            child: Image.network(
+                                              fit: BoxFit.cover,
+                                              data['photo'],
+                                              height: 150,
+                                              width: 300,
+                                            ),
+                                          ),
+                                    GestureDetector(
+                                      onTap: () async {
+                                        final docRef = FirebaseFirestore
+                                            .instance
+                                            .collection("services")
+                                            .doc(data['uuid']);
+                                        if (isFavorite) {
+                                          // Remove from favorites
+                                          await docRef.update({
+                                            "favorite": FieldValue.arrayRemove(
+                                                [currentUserId])
+                                          });
+                                        } else {
+                                          // Add to favorites
+                                          await docRef.update({
+                                            "favorite": FieldValue.arrayUnion(
+                                                [currentUserId])
+                                          });
+                                        }
+                                      },
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Align(
+                                          alignment:
+                                              AlignmentDirectional.topEnd,
+                                          child: Container(
+                                            child: Icon(
+                                              isFavorite
+                                                  ? Icons.favorite
+                                                  : Icons.favorite_outline,
+                                              color: red,
+                                            ),
+                                            height: 50,
+                                            width: 50,
+                                            decoration: BoxDecoration(
+                                                shape: BoxShape.circle,
+                                                color: colorWhite),
+                                          ),
+                                        ),
                                       ),
-                                title: Text(
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
                                   data['title'],
                                   style: GoogleFonts.inter(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16),
-                                ),
-                                subtitle: SizedBox(
-                                  width: 200,
-                                  height: 150,
-                                  child: Text(
-                                    data['description'],
-                                    style: GoogleFonts.inter(
-                                        color: Color(0xff9C9EA2),
-                                        fontWeight: FontWeight.w300,
-                                        fontSize: 15),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
                                   ),
                                 ),
                               ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Column(
-                                    children: [
-                                      Text(
-                                        "${getCurrencySymbol(data['currency'] ?? 'Euro')}${data['price'] ?? '0.0'}",
-                                        style: GoogleFonts.inter(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20,
+                              Padding(
+                                padding: const EdgeInsets.all(3.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(
+                                          Icons.star,
+                                          color: yellow,
                                         ),
+                                        Text(
+                                          data['totalReviews'].toString(),
+                                          style: GoogleFonts.inter(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 13),
+                                        ),
+                                      ],
+                                    ),
+                                    Text(
+                                      "${getCurrencySymbol(data['currency'] ?? 'Euro')}${data['price'] ?? '0.0'}",
+                                      style: GoogleFonts.inter(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
                                       ),
-                                      Text(
-                                        data['priceType'],
-                                        style: GoogleFonts.inter(
-                                            color: Color(0xff9C9EA2),
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 19),
-                                      ),
-                                    ],
-                                  ),
-                                  Image.asset(
-                                    "assets/line.png",
-                                    height: 40,
-                                    width: 52,
-                                  ),
-                                  Column(
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Icon(
-                                            Icons.star,
-                                            color: yellow,
-                                          ),
-                                          Text(
-                                            data['totalReviews'].toString(),
-                                            style: GoogleFonts.inter(
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 20),
-                                          ),
-                                        ],
-                                      ),
-                                      Text(
-                                        data['ratingCount'].toString() +
-                                            " Reviews",
-                                        style: GoogleFonts.inter(
-                                            color: Color(0xff9C9EA2),
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 19),
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              )
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ],
                           ),
-                        );
-                      }),
-                );
+                        ),
+                      ),
+                    );
+                  },
+                ));
               }),
         ],
       ),
