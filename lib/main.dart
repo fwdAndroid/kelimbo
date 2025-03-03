03e94773a1eb975b8c8eee99da6ed6dcb27a6292
fwdAndroid <fwdkaleem@gmail.com>
2025-03-03 17:26:35 +0500

    Route Adjusted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:kelimbo/screens/auth/profile_page_1.dart';
 import 'package:kelimbo/screens/splash_screen.dart';
 
 void main() async {
@@ -36,7 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProfilePage1(),
+      home: const SplashScreen(),
     );
   }
 }
